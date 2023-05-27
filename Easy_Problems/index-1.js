a9954d442c4aee7abbb08228f7ace038fab503d6
Govind <govindsvyadav@gmail.com>
2023-05-27 11:22:53 +0530

    counter

---
+++ Easy_Problems/index-1.js
@@ -0,0 +1,13 @@
+function createCounter(n){
+
+    return function(){
+       return n++ ;
+    }
+}
+
+const counter = createCounter(10);
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
