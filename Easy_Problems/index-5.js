28d94983421a85eace82856ddc4ea72fc5805282
9ovindyadav <govindsvyadav@gmail.com>
2023-05-28 22:27:54 +0530

    Two sum

---
+++ Easy_Problems/index-5.js
@@ -0,0 +1,27 @@
+// Problem - Two sum
+
+//Given an array of integers nums and an integer target, 
+//return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+
+function twoSum(nums, target) {
+    const numMap = new Map();
+  
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (numMap.has(complement)) {
+        return [numMap.get(complement), i];
+      }
+      numMap.set(nums[i], i);
+    }
+  
+    return [];
+  }
+  
+  // Example usage
+  const nums = [2, 7, 11, 15];
+  const target = 9;
+  const result = twoSum(nums, target);
+  console.log(result);
